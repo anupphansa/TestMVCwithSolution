96039adc1a448ebfe4f8fcc7c6a24603bfc46f03
Anup Patel <anup@Annetsite.intranet>
2019-05-21 20:03:00 +0530

    commit on server

---
+++ TestMVCwithGitHub/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TestMVCwithGitHub.Models;
+
+namespace TestMVCwithGitHub.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(Employee emp )
+        {
+            if (ModelState.IsValid == true)
+            {
+
+                return RedirectToAction("success", "Home");
+            }
+
+            return View(emp);
+        }
+
+        public ActionResult success()
+        {
+            return View();
+        }
+    }
+}
---
+++ TestMVCwithGitHub/Models/Employee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace TestMVCwithGitHub.Models
+{
+    public class Employee
+    {
+
+        public int EmployeeId { get; set; }
+        
+        [Required]
+        [Display(Name="First Name")]
+        public string EmpFirstName { get; set; }
+        [Required]
+        [Display(Name = "Last Name")]
+        public string EmpLastName { get; set; }
+    }
+}
